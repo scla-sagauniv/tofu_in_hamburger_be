07c282e8dfe97be307a471173ee38cf09e571246
shunsuke-tamura <wwwxx4869@gmail.com>
2023-06-17 01:12:24 +0900

    update: dbのhostをenvに追加

--- app/db/db.go
+++ app/db/db.go
@@ -12,9 +12,10 @@ var Db *sql.DB
 func Init() {
 	db_user := os.Getenv("MYSQL_USER")
 	db_password := os.Getenv("MYSQL_PASSWORD")
+	db_host := os.Getenv("MYSQL_HOST")
 	db_port := os.Getenv("MYSQL_PORT")
 	db_database := os.Getenv("MYSQL_DATABASE")
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(db:%s)/%s?charset=utf8&parseTime=true", db_user, db_password, db_port, db_database)
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true", db_user, db_password, db_host, db_port, db_database)
 
 	var err error
 	Db, err = sql.Open("mysql", dataSourceName)
