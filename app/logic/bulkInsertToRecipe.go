a9acef1a12cf7cd05e08576a94e13f6082a6fb4e
shunsuke-tamura <wwwxx4869@gmail.com>
2023-06-18 14:55:38 +0900

    mysqlに戻した

--- app/logic/bulkInsertToRecipe.go
+++ app/logic/bulkInsertToRecipe.go
@@ -29,28 +29,29 @@ func boolToInt(val bool) int {
 
 func BulkInsertToRecipe(recipes []*v1.RecipeOnDb) error {
 	log.Println("Start BulkInsertToRecipe")
-	insert := "INSERT INTO recipes (id, title, recipe_url, image_url, pickup, nickname, materials, material_ids, publishday, ranking, recipe_indication_id, recipe_cost_id, created_at, updated_at) VALUES "
+	insert := "INSERT INTO recipes (id, title, recipe_url, image_url, pickup, nickname, materials, material_ids, publishday, ranking, recipe_indication_id, recipe_cost_id) VALUES "
+	// insert := "INSERT INTO recipes (id, title, recipe_url, image_url, pickup, nickname, materials, material_ids, publishday, ranking, recipe_indication_id, recipe_cost_id, created_at, updated_at) VALUES "
 
 	var vals []any
 	for _, recipe := range recipes {
-		// insert += fmt.Sprintf(`(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?), `)
+		insert += fmt.Sprintf(`(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?), `)
 		publishday, err := timestampToString(recipe.GetPublishday())
 		if err != nil {
 			log.Fatal(err)
 			return err
 		}
-		createdAt, err := timestampToString(recipe.GetCreatedAt())
-		if err != nil {
-			log.Fatal(err)
-			return err
-		}
-		updatedAt, err := timestampToString(recipe.GetUpdatedAt())
-		if err != nil {
-			log.Fatal(err)
-			return err
-		}
-		// vals = append(vals, recipe.GetId(), recipe.GetTitle(), recipe.GetRecipeUrl(), recipe.GetImageUrl(), recipe.GetPickup(), recipe.GetNickname(), recipe.GetMaterials(), recipe.GetMaterialIds(), mysqlDateTime, recipe.GetRanking(), recipe.GetRecipeIndicationId(), recipe.GetRecipeCostId())
-		insert += fmt.Sprintf(`(%d, '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s', %d, %d, %d, '%s', '%s'), `, recipe.GetId(), recipe.GetTitle(), recipe.GetRecipeUrl(), recipe.GetImageUrl(), boolToInt(recipe.GetPickup()), recipe.GetNickname(), recipe.GetMaterials(), recipe.GetMaterialIds(), publishday, recipe.GetRanking(), recipe.GetRecipeIndicationId(), recipe.GetRecipeCostId(), createdAt, updatedAt)
+		// createdAt, err := timestampToString(recipe.GetCreatedAt())
+		// if err != nil {
+		// 	log.Fatal(err)
+		// 	return err
+		// }
+		// updatedAt, err := timestampToString(recipe.GetUpdatedAt())
+		// if err != nil {
+		// 	log.Fatal(err)
+		// 	return err
+		// }
+		vals = append(vals, recipe.GetId(), recipe.GetTitle(), recipe.GetRecipeUrl(), recipe.GetImageUrl(), recipe.GetPickup(), recipe.GetNickname(), recipe.GetMaterials(), recipe.GetMaterialIds(), publishday, recipe.GetRanking(), recipe.GetRecipeIndicationId(), recipe.GetRecipeCostId())
+		// insert += fmt.Sprintf(`(%d, '%s', '%s', '%s', %d, '%s', '%s', '%s', '%s', %d, %d, %d, '%s', '%s'), `, recipe.GetId(), recipe.GetTitle(), recipe.GetRecipeUrl(), recipe.GetImageUrl(), boolToInt(recipe.GetPickup()), recipe.GetNickname(), recipe.GetMaterials(), recipe.GetMaterialIds(), publishday, recipe.GetRanking(), recipe.GetRecipeIndicationId(), recipe.GetRecipeCostId(), createdAt, updatedAt)
 	}
 	insert = insert[:len(insert)-2]
 
